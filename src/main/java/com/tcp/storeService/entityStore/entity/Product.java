d7103abb814a6480fdae6682a809b5ccf648c537
swapinal.j <swapinal.j@BBMQASWAPINALJ.internal.zeta.in>
2019-10-03 17:48:43 +0530

    Product Beans

--- src/main/java/com/tcp/storeService/entityStore/entity/Product.java
+++ src/main/java/com/tcp/storeService/entityStore/entity/Product.java
@@ -1,5 +1,22 @@
 package com.tcp.storeService.entityStore.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
 public class Product {
 
+	@Id
+	@GeneratedValue
+	private long productID;
+	
+	private String productName;
+	private String type;
+	private String description;
+	private long createdAt;
+	private long modifiedAt;
 }
