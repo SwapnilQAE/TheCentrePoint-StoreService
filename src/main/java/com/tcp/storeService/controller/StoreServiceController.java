3e3ff6d164cf1bcb8e41acf58c77571707f51633
swapinal.j <swapinal.j@BBMQASWAPINALJ.internal.zeta.in>
2019-10-09 13:28:11 +0530

    Adding swagger and some exception handling in code

--- src/main/java/com/tcp/storeService/controller/StoreServiceController.java
+++ src/main/java/com/tcp/storeService/controller/StoreServiceController.java
@@ -1,24 +1,18 @@
 package com.tcp.storeService.controller;
 
+import com.tcp.storeService.dao.Store;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.tcp.storeService.dao.Store;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-@Api(value = "The Center Point")
-@RestController
 @Validated
+@RestController
+@Api(value = "The Center Point")
 public class StoreServiceController {
 
     @ApiOperation(value = "API to create a new store in the system", httpMethod = "POST", response = String.class)
@@ -26,8 +20,8 @@ public class StoreServiceController {
     public void createStore(@RequestBody Store newStore) {
     }
 
-    @ApiOperation(value = "Api to retrive the store details for given store ID", httpMethod = "GET", response = String.class)
-    @RequestMapping(value = "/getStroe", method = RequestMethod.GET)
+    @ApiOperation(value = "Api to retrieve the store details for given store ID", httpMethod = "GET", response = String.class)
+    @RequestMapping(value = "/getStore", method = RequestMethod.GET)
     public String getStore(@ApiParam(name = "storeID", value = "Store Id to get the store details", required = true)
                            @RequestParam(name = "storeID", required = true)
                            @Min(value = 1, message = "Store Id must be greater then 1")
